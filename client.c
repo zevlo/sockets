f473b79a61d0e1ea84255153897d9a61119c91fd
zevlo <zachallen21@gmail.com>
2025-12-06 17:14:51 -0500

    Bind ports to 8080

--- client.c
+++ client.c
@@ -14,43 +14,51 @@ int main(int argc, char *argv[]) {
     char recvBuff[1024];
     struct sockaddr_in serv_addr;
 
+    // Validate Arguments
     if(argc != 2) {
         printf("\n Usage: %s <ip of server> \n", argv[0]);
         return 1;
     }
 
-    memset(recvBuff, 0, sizeof(recvBuff));
+    // Create Socket
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Error : Could not create socket \n");
+        perror("Socket creation failed");
         return 1;
     }
     
     memset(&serv_addr, 0, sizeof(serv_addr));
+    memset(recvBuff, 0, sizeof(recvBuff));
+
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(5000);
+    serv_addr.sin_port = htons(8080); 
+
+    // Convert IP Address
     if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0) {
-        printf("\n inet_pton error occured\n");
+        fprintf(stderr, "\nInvalid address/ Address not supported \n");
         close(sockfd);
         return 1;
     }
 
+    // Connect to Server
     if(connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\n Error : Connect Failed \n");
+        perror("Connect Failed");
         close(sockfd);
         return 1;
     }
 
+    // Read Response
     while((n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0) {
         recvBuff[n] = 0;
         if(fputs(recvBuff, stdout) == EOF) {
-            printf("\n Error : Fputs error\n");
+            perror("\n Error : Fputs error\n");
         }
-        printf("\n");
+        printf("\n"); 
     }
 
     if(n < 0) {
-        printf("\n Read error \n");
+        perror("\n Read error \n");
     }
+
     close(sockfd);
     return 0;
-}  
+}
